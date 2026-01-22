a6ed01e36448c029d188afc171d72fb87d4393eb
Mike Denney <mike.a.denney@gmail.com>
2026-01-21 19:43:29 -0600

    initial group work

---
+++ src/main/java/com/example/mycollections/controllers/AlbumController.java
@@ -0,0 +1,47 @@
+package com.example.mycollections.controllers;
+
+import com.example.mycollections.models.Album;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private final List<Album> albums = new ArrayList<>() {{
+        add(new Album("Hot Fuss", "The Killers", 2004, 12));
+        add(new Album("25", "Adele", 2015, 14));
+        add(new Album("Abbey Road", "The Beatles", 1969, 21));
+    }};
+
+    @GetMapping("/json")
+    public List<Album> getAlbumsJson() {
+        return albums;
+    }
+
+    @GetMapping("/html")
+    public String getAlbumsHtml() {
+        String albumList = "<ul>";
+        for (Album album : albums) {
+            albumList += "<li>" + album + "</li>";
+        }
+        albumList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Albums</h1>
+                        <ul>
+                """ +
+                albumList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
+
+}
---
+++ src/main/java/com/example/mycollections/controllers/MovieController.java
@@ -0,0 +1,47 @@
+package com.example.mycollections.controllers;
+
+import com.example.mycollections.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final List<Movie> movies = new ArrayList<>() {{
+        add(new Movie("The Great Gatsby", "Baz Luhrmann", 2013, 142));
+        add(new Movie("Zootopia", "Byron Howard", 2016, 122));
+        add(new Movie("The Lion King", "Roger Allers", 1994, 97));
+    }};
+
+    @GetMapping("/json")
+    public List<Movie> getMoviesJson() {
+        return movies;
+    }
+
+    @GetMapping("/html")
+    public String getMoviesHtml() {
+        String movieList = "<ul>";
+        for (Movie movie : movies) {
+            movieList += "<li>" + movie + "</li>";
+        }
+        movieList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Movies</h1>
+                        <ul>
+                """ +
+                movieList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
+
+}
